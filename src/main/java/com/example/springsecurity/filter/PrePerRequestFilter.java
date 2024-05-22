36cb637a9c558b7bc13747c9a0a9942e9ee24991
negin <negin.s.mousavi@gmail.com>
2024-05-22 15:46:07 +0330

    feat: add filter to put correlationId for any requests in log

---
+++ src/main/java/com/example/springsecurity/filter/PrePerRequestFilter.java
@@ -0,0 +1,35 @@
+package com.example.springsecurity.filter;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PrePerRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        MDC.put("correlationId", UUID.randomUUID().toString().toUpperCase().replace("-", ""));
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.contains("swagger") || path.contains("api-docs");
+    }
+}
